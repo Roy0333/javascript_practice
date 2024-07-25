2463cc4e9d147ca1fb962e6dc38001c172096995
Angshuman Roy <akashrou72@gmail.com>
2024-07-26 00:44:12 +0530

    for and while loop added

---
+++ Loops/02_WhileLoop.js
@@ -0,0 +1,13 @@
+// SYNTAX:
+// while (condition) {
+    
+// }
+
+// EXAMPLE:
+let i = 0;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+// (we can  not use const while diclearing variable for while loop)
