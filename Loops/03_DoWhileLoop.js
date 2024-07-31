62007452a45750057bdd90066479624115721c05
Angshuman Roy <akashrou72@gmail.com>
2024-07-31 23:32:40 +0530

    for of loop added

---
+++ Loops/03_DoWhileLoop.js
@@ -0,0 +1,13 @@
+// SYNTAX:
+// do {
+
+// } while (condition);
+
+// EXAMPLE:
+let score = 1;
+do {
+  console.log(`My Score is ${score}`);
+  score++;
+} while (score <= 10);
+
+// even not conditions checked this loop provides us a value initially
---
+++ Loops/04_ForOfLoop.js
@@ -0,0 +1,10 @@
+// SYNTAX:
+// for (const iterator of object) {
+
+// }
+
+// EXAMPLE:
+const myHero = ["superman", "batman", "spiderman", "thor", "captain america"];
+for (const i of myHero) {
+  console.log(i);
+}
